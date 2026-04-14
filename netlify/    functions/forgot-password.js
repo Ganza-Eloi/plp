2a574638882b3cb8f25d7ead92729fce2655287c
Ganza-Eloi <ganzaeloi2010@gmail.com>
2026-04-14 07:51:08 +0200

    Update forgot-password.js

--- netlify/    functions/forgot-password.js
+++ netlify/    functions/forgot-password.js
@@ -1,1 +1,9 @@
-
+exports.handler = async () => {
+  return {
+    statusCode: 403,
+    body: JSON.stringify({
+      success: false,
+      message: "Password reset is disabled. Use the allowed login credentials.",
+    }),
+  };
+};

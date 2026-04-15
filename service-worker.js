eb7c33bc0aabbcbacb06156fa2213c8ec252b700
Ganza-Eloi <ganzaeloi2010@gmail.com>
2026-04-15 19:44:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,23 @@
-         self.addEventListener("install", event => {
-    console.log("service worker installed");
+const CACHE_NAME ="Urugwiro-cache-v1";
+
+const urlsToCache = [
+         "/",
+         "/index.html",
+         "/style.css",
+         "/script.js",
+         "/images/favicon.png",
+         "/images/favicon.png"
+         ];
+self.addEventListener("install", event => {
+         event.waitUntil(
+                  caches.open(CACHE_NAME)
+                  .then(cache => cache.addAll(urlsToCache))
+                  );
 });
 self.addEventListener("fetch", event => {
-    event.respondWidth(fetch(event.request));
+         event.respondWith(
+                  caches.match(event.request)
+                  .then(response => response ||
+                           fetch(event.request))
+                  );
 });

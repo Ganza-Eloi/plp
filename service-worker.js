c64fe5cf306c9c02cb071c36537046c9c0373d2c
Ganza-Eloi <ganzaeloi2010@gmail.com>
2026-04-16 14:57:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,47 @@
-const CACHE_NAME ="saintjosephplp-cache-v1";
+const CACHE_NAME = "saintjosephplp-cache-v1";
 
 const urlsToCache = [
-         "/",
-         "/index.html",
-         "/style.css",
-         "/script.js",
-         "/images/icon-192.png",
-         "/images/icon-512.png"
-         ];
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/images/icon-192.png",
+  "/images/icon-512.png"
+];
+
+// INSTALL
 self.addEventListener("install", event => {
-         event.waitUntil(
-                  caches.open(CACHE_NAME)
-                  .then(cache => cache.addAll(urlsToCache))
-                  );
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clientsClaim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
-         event.respondWith(
-                  caches.match(event.request)
-                  .then(response => response ||
-                           fetch(event.request))
-                  );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
